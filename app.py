d70134d91ea9459878f6f8cd6eca8374728349d0
raj-2093 <lunagariyarajy2093@gmail.com>
2023-12-27 15:40:37 +0530

    added total count feature

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-def get_linkedin_post_likes(post_url):
+def get_linkedin_post_reactions_details(post_url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
@@ -17,56 +17,37 @@ def get_linkedin_post_likes(post_url):
         page_title = soup.find('h1')
 
         like_count_element = soup.find('span', {'data-test-id' :'social-actions__reaction-count'})
+        comment_count_element = soup.find('a', {'data-test-id' :'social-actions__comments'})
+        sum=0
 
         if(page_title.text.__contains__("Post")):
             # Find the element containing the like count
             if like_count_element:
                 like_count = like_count_element.text
+                like_count = "".join(like_count.split(","))
+                sum = sum + int(like_count) 
                 print(f"Like Count: {like_count}")
             else:
                 like_count = "0"
+            
+            if comment_count_element:
+                comment_count = comment_count_element.text.split()[0]
+                sum = sum + int(comment_count)
+                print(f"Comment Count: {comment_count}")
+            else:
+                comment_count = "0"
 
         else:
             like_count = "Maybe Private Account ⚠️ OR\n Post Deleted 🥸"
-
-        # WORKING
-        return like_count
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
-    
-def get_linkedin_post_comments(post_url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    try:
-        response = requests.get(post_url, headers=headers)
-        response.raise_for_status()
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-        page_title = soup.find('h1')
-
-        comment_count_element = soup.find('a', {'data-test-id' :'social-actions__comments'})
-
-        if(page_title.text.__contains__("Post")):
-            # Find the element containing the like count
-            if comment_count_element:
-                comment_count = comment_count_element.text
-                print(f"Comment Count: {comment_count}")
-            else:
-                comment_count = "0 Comment"
-
-        else:
             comment_count = "Maybe Private Account ⚠️ OR\n Post Deleted 🥸"
+            sum = "Maybe Private Account ⚠️ OR\n Post Deleted 🥸"
 
         # WORKING
-        return comment_count
+        return [like_count,comment_count,sum]
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return None
-
 
 @app.route('/')
 def index():
@@ -74,9 +55,8 @@ def index():
     with open(file_path, 'r', encoding='utf-8') as json_file:
         post_data = json.load(json_file)
 
-    like_counts = [get_linkedin_post_likes(data["postUrl"]) for data in post_data]
-    comment_counts = [get_linkedin_post_comments(data["postUrl"]) for data in post_data]
-    return render_template('index.html', post_data=post_data, like_counts=like_counts,comment_counts=comment_counts,zip = zip)
+    post_reactions = [get_linkedin_post_reactions_details(data["postUrl"]) for data in post_data]
+    return render_template('index.html', post_data=post_data, post_reactions=post_reactions,zip = zip)
 
 if __name__ == '__main__':
     app.run(debug=True)
